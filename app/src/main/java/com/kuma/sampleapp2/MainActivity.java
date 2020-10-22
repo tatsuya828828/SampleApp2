118246525c88efbe89efb07f7b4920447cd85d59
tatsuya828828 <kkjy011894@gmail.com>
2020-10-22 14:55:32 +0900

    ボタンを押した時の処理内容を変更

--- app/src/main/java/com/kuma/sampleapp2/MainActivity.java
+++ app/src/main/java/com/kuma/sampleapp2/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.Date;
@@ -14,6 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.btnCurrent);
+        btn.setOnClickListener(
+            new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    TextView txt = findViewById(R.id.txtResult);
+                    txt.setText(new Date().toString());
+                }
+            }
+        );
     }
 
     public  void btnCurrent_onClick(View view) {
