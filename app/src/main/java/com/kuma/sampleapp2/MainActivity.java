5d4c3b6c7cd7d20eae7d65925db145249c91e21f
tatsuya828828 <kkjy011894@gmail.com>
2020-10-22 14:19:45 +0900

    buttonを押した際に値を代入する処理を追加

--- app/src/main/java/com/kuma/sampleapp2/MainActivity.java
+++ app/src/main/java/com/kuma/sampleapp2/MainActivity.java
@@ -3,6 +3,10 @@ package com.kuma.sampleapp2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public  void btnCurrent_onClick(View view) {
+        TextView txt = findViewById(R.id.txtResult);
+        txt.setText(new Date().toString());
     }
 }
