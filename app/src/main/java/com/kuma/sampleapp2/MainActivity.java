6a407b3fad2812b42d33dad3f457d873f993360b
tatsuya828828 <kkjy011894@gmail.com>
2020-10-23 11:01:35 +0900

    画面回転時に状態を保持する処理を追加

--- app/src/main/java/com/kuma/sampleapp2/MainActivity.java
+++ app/src/main/java/com/kuma/sampleapp2/MainActivity.java
@@ -27,4 +27,20 @@ public class MainActivity extends AppCompatActivity {
         TextView txt = findViewById(R.id.txtResult);
         txt.setText(new Date().toString());
     }
+
+    // 画面が破棄される前の状態を保存
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        TextView txtResult = findViewById(R.id.txtResult);
+        outState.putString("txtResult", txtResult.getText().toString());
+    }
+
+    // 画面が復元される際に状態を取り出し
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        TextView txtResult = findViewById(R.id.txtResult);
+        txtResult.setText(savedInstanceState.getString("txtResult"));
+    }
 }
