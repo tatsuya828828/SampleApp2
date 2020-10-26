aacf696733d0cd697716a576de6ee8ab7868c4dd
tatsuya828828 <kkjy011894@gmail.com>
2020-10-26 23:09:10 +0900

    ImageButtonクリック時の処理を追加

--- app/src/main/java/com/kuma/sampleapp2/MainActivity.java
+++ app/src/main/java/com/kuma/sampleapp2/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,6 +19,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageButton imageButton = findViewById(R.id.imageButton);
+        imageButton.setOnClickListener(
+            new View.OnClickListener() {
+                @Override
+                public void onClick(View view){
+                    btnSend_onClick(view);
+                }
+            }
+        );
+    }
 
+    // ボタンクリック時に入力された名前を表示
+    public void btnSend_onClick(View view) {
+        EditText txtName = findViewById(R.id.txtName);
+        TextView txtResult = findViewById(R.id.txtResult);
+        txtResult.setText(String.format("ようこそ、%sさん！", txtName.getText()));
     }
 }
