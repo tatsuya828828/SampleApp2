3953ede76d99a08429e1879ea491f7b83f8b15bc
tatsuya828828 <kkjy011894@gmail.com>
2020-10-27 22:14:18 +0900

    チェックの状態によりトーストを表示する処理を追加

--- app/src/main/java/com/kuma/sampleapp2/MainActivity.java
+++ app/src/main/java/com/kuma/sampleapp2/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -19,5 +21,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // チェックボックスを取得
+        CheckBox checkBox = findViewById(R.id.checkBox);
+        // チェックボックスに対してイベントリスナーを登録
+        checkBox.setOnCheckedChangeListener(
+                //  CompoundButtonオブジェクトは、チェックボックスやラジオボタン、トグルボタンなど、
+                //  オンオフの状態を持つボタンのスーパークラス
+                new CompoundButton.OnCheckedChangeListener() {
+                    @Override
+                    // checkの状態が引数のisCheckedに渡される
+                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                        Toast.makeText(MainActivity.this,
+                                // isCheckedの値に応じて文字列をトースト表示
+                                isChecked ? "メール送信をオン" : "メール送信をオフ",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
     }
 }
