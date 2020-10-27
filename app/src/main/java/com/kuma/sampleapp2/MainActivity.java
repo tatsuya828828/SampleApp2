8e5ae401132c86efece227a1b3d8dc2aa1e68b62
tatsuya828828 <kkjy011894@gmail.com>
2020-10-27 21:54:49 +0900

    ボタンとボタン用処理を削除

--- app/src/main/java/com/kuma/sampleapp2/MainActivity.java
+++ app/src/main/java/com/kuma/sampleapp2/MainActivity.java
@@ -19,21 +19,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageButton imageButton = findViewById(R.id.imageButton);
-        imageButton.setOnClickListener(
-            new View.OnClickListener() {
-                @Override
-                public void onClick(View view){
-                    btnSend_onClick(view);
-                }
-            }
-        );
-    }
-
-    // ボタンクリック時に入力された名前を表示
-    public void btnSend_onClick(View view) {
-        EditText txtName = findViewById(R.id.txtName);
-        TextView txtResult = findViewById(R.id.txtResult);
-        txtResult.setText(String.format("ようこそ、%sさん！", txtName.getText()));
     }
 }
