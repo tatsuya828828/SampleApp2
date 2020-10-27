d743234131977630f11032a4bdce9132debfbf1b
tatsuya828828 <kkjy011894@gmail.com>
2020-10-27 22:48:02 +0900

    ラジオボタンの選択内容によりトーストを表示する処理を追加

--- app/src/main/java/com/kuma/sampleapp2/MainActivity.java
+++ app/src/main/java/com/kuma/sampleapp2/MainActivity.java
@@ -10,6 +10,8 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -24,19 +26,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // チェックボックスを取得
-        Switch switch1 = findViewById(R.id.switch1);
-        // チェックボックスに対してイベントリスナーを登録
-        switch1.setOnCheckedChangeListener(
-                //  CompoundButtonオブジェクトは、チェックボックスやラジオボタン、トグルボタンなど、
-                //  オンオフの状態を持つボタンのスーパークラス
-                new CompoundButton.OnCheckedChangeListener() {
+        // radioGroupを取得
+        RadioGroup radioGroup = findViewById(R.id.radioGroup);
+        // radioGroupに対してイベントリスナーを登録
+        radioGroup.setOnCheckedChangeListener(
+                new RadioGroup.OnCheckedChangeListener() {
                     @Override
-                    // checkの状態が引数のisCheckedに渡される
-                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    // 選択状態に応じてトーストを表示
+                    public void onCheckedChanged(RadioGroup group, int checkedId) {
+                        RadioButton radio = group.findViewById(checkedId);
                         Toast.makeText(MainActivity.this,
                                 // isCheckedの値に応じて文字列をトースト表示
-                                isChecked ? "メール送信をオン" : "メール送信をオフ",
+                                String.format("[%s]が選択されました。", radio.getText()),
                                 Toast.LENGTH_SHORT).show();
                     }
                 }
