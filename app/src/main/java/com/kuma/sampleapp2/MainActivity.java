74118d4774c0f44ffa0937d4f6f477010dc6137e
tatsuya828828 <kkjy011894@gmail.com>
2020-10-28 19:50:19 +0900

    シークバーの値によってトーストを表示

--- app/src/main/java/com/kuma/sampleapp2/MainActivity.java
+++ app/src/main/java/com/kuma/sampleapp2/MainActivity.java
@@ -12,12 +12,14 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.SeekBar;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,21 +28,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // radioGroupを取得
-        RadioGroup radioGroup = findViewById(R.id.radioGroup);
-        // radioGroupに対してイベントリスナーを登録
-        radioGroup.setOnCheckedChangeListener(
-                new RadioGroup.OnCheckedChangeListener() {
-                    @Override
-                    // 選択状態に応じてトーストを表示
-                    public void onCheckedChanged(RadioGroup group, int checkedId) {
-                        RadioButton radio = group.findViewById(checkedId);
-                        Toast.makeText(MainActivity.this,
-                                // isCheckedの値に応じて文字列をトースト表示
-                                String.format("[%s]が選択されました。", radio.getText()),
-                                Toast.LENGTH_SHORT).show();
-                    }
-                }
-        );
+        // SeekBarを取得
+        SeekBar seekBar = findViewById(R.id.seekBar);
+        // SeekBarに対してイベントリスナーを登録
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            // 現在地に応じてトーストを表示
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Toast.makeText(MainActivity.this, String.format(Locale.JAPAN, "現在地:%d", progress)
+                    , Toast.LENGTH_SHORT).show();
+            }
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
     }
 }
