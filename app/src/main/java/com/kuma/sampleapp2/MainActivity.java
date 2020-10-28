19e8415eccf32aae1f2c8d3835ffda912e0333d9
tatsuya828828 <kkjy011894@gmail.com>
2020-10-29 00:24:15 +0900

    スピナーに値を登録するメソッドを追加

--- app/src/main/java/com/kuma/sampleapp2/MainActivity.java
+++ app/src/main/java/com/kuma/sampleapp2/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        // Spinnerに項目リストを登録するメソッドの呼び出し
         createSpinner();
     }
 
